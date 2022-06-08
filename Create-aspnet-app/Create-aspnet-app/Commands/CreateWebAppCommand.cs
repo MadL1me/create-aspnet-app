8c4f1d06b2860f840362eec01e97c755ede84575
Ilya Klimenko <46647517+Sn0wy-0wl@users.noreply.github.com>
2022-06-09 00:44:42 +0300

    Added extensions, added version checking logic

--- Create-aspnet-app/Create-aspnet-app/Commands/CreateWebAppCommand.cs
+++ Create-aspnet-app/Create-aspnet-app/Commands/CreateWebAppCommand.cs
@@ -3,6 +3,7 @@ using CliFx.Attributes;
 using CliFx.Infrastructure;
 using CliWrap;
 using CliWrap.Buffered;
+using CliWrap.Exceptions;
 using Create_aspnet_app.Utils;
 using Sharprompt;
 using Sharprompt.Prompts;
@@ -19,9 +20,13 @@ public class BuilderData
 [Command]
 public class CreateWebAppCommand : ICommand
 {
+    public const string AppVersion = "0.0.2";
+    public const string AspAwesomeTemplateRequiredVersion = "0.0.2";
+    
     public const string DotnetCommandName = "dotnet";
     public const string DotnetNewCommandName = "new";
-    public const string TemplateShortName = "ez-webapp-main";
+    public const string TemplateShortName = "asp-awesome-main-cli";
+    public const string TemplatePackageId = "Asp.AwesomeTemplates.Main.Cli";
     
     private readonly BuilderData _builderData = new () { CliArguments = { DotnetNewCommandName, TemplateShortName } };
     
@@ -31,12 +36,30 @@ public class CreateWebAppCommand : ICommand
 
         await console.Output.WriteLineAsyncWithColors("Welcome To Create-aspnet-app!", ConsoleColor.DarkCyan);
 
-        await ConfigureName(prompt, _builderData);
-        await RunDotnetNewCommand(console);
+        if (await CheckIfTemplateExists(console))
+        { 
+            if (await GetTemplateVersion(console) != AspAwesomeTemplateRequiredVersion)
+                await InstallRequiredTemplateVersion(console);
+        }
+        else await InstallRequiredTemplateVersion(console);
+
+        //await ConfigureName(prompt, _builderData);
+        //await RunDotnetNewCommand(console);
 
         await console.Output.WriteLineAsync($"Your project {_builderData.ProjectName} was successfully created!");
     }
 
+    private async ValueTask InstallRequiredTemplateVersion(IConsole console)
+    {
+        const string install = "--install";
+        
+        var commandResult = await Cli.Wrap($"{DotnetCommandName}")
+            .WithArguments(new [] {DotnetNewCommandName, install, TemplatePackageId + "::" + AspAwesomeTemplateRequiredVersion})
+            .ExecuteBufferedAsync();
+
+        await console.Output.WriteAsync(commandResult.StandardOutput);
+    }
+    
     private ValueTask ConfigureName(IPrompt prompt, BuilderData data)
     {
         const string optionName = "-o";
@@ -44,6 +67,41 @@ public class CreateWebAppCommand : ICommand
         data.CliArguments.Add(optionName);
         data.CliArguments.Add(projectName);
         return default;
+    }
+    
+    private async ValueTask<string> GetTemplateVersion(IConsole console)
+    {
+        const string helpArg = "-u";
+        
+        var commandResult = await Cli.Wrap($"{DotnetCommandName}")
+            .WithArguments(new [] {DotnetNewCommandName, helpArg})
+            .ExecuteBufferedAsync();
+        
+        var lines = commandResult.StandardOutput.Split("\n");
+        var nameNumber = lines.IndexOfOrNull(s => s.Contains(TemplatePackageId));
+
+        if (nameNumber is null)
+            throw new KeyNotFoundException("Founded value is not found");
+        
+        var version = lines[nameNumber.Value + 1]
+            .Split(Array.Empty<char>(), StringSplitOptions.RemoveEmptyEntries)[1];
+
+        return version;
+    }
+    
+    private async ValueTask<bool> CheckIfTemplateExists(IConsole console)
+    {
+        const string helpArg = "-h";
+        
+        var commandResult = await Cli.Wrap($"{DotnetCommandName}")
+            .WithArguments(new [] {DotnetNewCommandName, TemplateShortName, helpArg})
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteBufferedAsync();
+        
+        await console.Output.WriteAsync(commandResult.StandardOutput);
+        await console.Output.WriteAsync(commandResult.StandardError);
+
+        return commandResult.StandardError.Length != 0;
     }
 
     private async ValueTask RunDotnetNewCommand(IConsole console)
--- Create-aspnet-app/Create-aspnet-app/Utils/Extensions.cs
+++ Create-aspnet-app/Create-aspnet-app/Utils/Extensions.cs
@@ -6,6 +6,13 @@ public static class Extensions
 {
     public static string WrapInQuotes(this string str) => $"\"{str}\"";
 
+    /// <summary>
+    /// Writes console with color of background and text
+    /// </summary>
+    /// <param name="writer">source</param>
+    /// <param name="value">what to write to console</param>
+    /// <param name="foregroundColor">text color</param>
+    /// <param name="backgroundColor">background color</param>
     public static async Task WriteLineAsyncWithColors(this ConsoleWriter writer, string value, 
         ConsoleColor foregroundColor = ConsoleColor.Gray, ConsoleColor backgroundColor = ConsoleColor.Black)
     {
@@ -19,5 +26,24 @@ public static class Extensions
 
         writer.Console.BackgroundColor = originalBackgroundColor;
         writer.Console.ForegroundColor = originalForegroundColor;
+    }
+
+    /// <summary>
+    /// Returns index of first successful predicate or null if not found any
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static int? IndexOfOrNull<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+    {
+        var index = 0;
+
+        foreach (var element in source)
+        {
+            if (predicate(element))
+                return index;
+
+            index++;
+        }
+
+        return null;
     }
 }
